51785e22af5ede7a23a616345123c2b8ff50dd49
Thon Ly <thonly@heartbank.ceo>
2020-08-19 19:21:47 -0700

    update module architecture

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { Input } from "./elements/sw-input.mjs"
-import { Signup } from "./components/sw-signup.mjs"
+import { Signup } from "./components/hb-signup.mjs"
 
 customElements.define("sw-input", Input, {extends: "input"})
 customElements.define("sw-signup", Signup)
