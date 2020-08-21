890b04cb665673d8b7874a35887c998f2a268455
Thon Ly <thonly@heartbank.ceo>
2020-08-20 21:34:31 -0700

    add tooltip element

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import { Input } from "./elements/sw-input.mjs"
 import { Signup } from "./components/hb-signup.mjs"
+import SwTooltip from "./elements/sw-tooltip.mjs"
 
 customElements.define("sw-input", Input, {extends: "input"})
-customElements.define("sw-signup", Signup)
+customElements.define("hb-signup", Signup)
+customElements.define("sw-tooltip", SwTooltip)
