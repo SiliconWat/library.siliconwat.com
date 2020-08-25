752c9507cb29302bf68d1abe210576b523e41580
Thon Ly <thonly@heartbank.ceo>
2020-08-24 19:17:23 -0700

    bug fix

--- render.mjs
+++ render.mjs
@@ -1,3 +1,5 @@
+// deprecated
+
 export function css(strings) {
     const style = document.createElement("style")
     style.textContent = strings
@@ -8,3 +10,32 @@ export function html(strings) {
     const template = new DOMParser().parseFromString(strings, "text/html")
     return template.documentElement.childNodes[1].firstChild
 } 
+
+/////
+
+//import {css, html} from "../render.mjs"
+
+export class HbSignup extends HTMLElement {
+    constructor() {
+        super()
+        const shadow = this.attachShadow({mode: "open"})
+        shadow.appendChild(this.style)
+        shadow.appendChild(this.markup)
+    }
+
+    get style() {
+        return css`
+            input {
+                outline: 2px solid green;
+            }
+        `
+    }
+
+    get markup() {
+        return html`
+            <form>
+                <input />
+            </form>
+        `
+    }
+}
