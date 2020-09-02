85d45386faa1037918ae9b3710e87804b5378c0c
Thon Ly <thonly@heartbank.ceo>
2020-09-01 21:53:46 -0700

    start sw-countdown element

--- elements/sw-countdown.mjs
+++ elements/sw-countdown.mjs
@@ -1,3 +1,5 @@
+import {SetInterval} from "../utils/SetTimeout.mjs"
+
 const template = document.createElement("template");
 template.innerHTML = `
     <style>
@@ -15,10 +17,35 @@ export class SwCountDown extends HTMLElement {
         this.shadowRoot.appendChild(template.content.cloneNode(true));
     }
 
+    connectedCallback() {
+        this.start()
+    }
+
     start() {
-        const hours = this.querySelector("h4 span")
-        const minutes = this.querySelector("input[name=minutes]")
-        const seconds = this.querySelector("input[name=seconds]")
+        const hours = this.querySelector("span:nth-child(1)")
+        const minutes = this.querySelector("span:nth-child(2)")
+        const seconds = this.querySelector("span:nth-child(3)")
+
+        //console.log(hours, minutes, seconds)
+
+        const timer = new SetInterval(() => {
+            if (hours.textContent == 0 && minutes.textContent == 0 && seconds.textContent == 0) {
+                timer.stop()
+            }
+            else {
+                if (seconds.textContent == 0) {
+                    seconds.textContent = 60
+                    if (minutes.textContent == 0) {
+                        hours.textContent = Number(hours.textContent) - 1
+                        minutes.textContent = 60
+                    }
+                    minutes.textContent = Number(minutes.textContent) - 1
+                }
+                seconds.textContent = Number(seconds.textContent) - 1
+            }
+        }, 1000)
+
+        timer.start()
     }
     
 }
