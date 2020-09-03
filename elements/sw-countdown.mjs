7b39e47b94e3cd22b4350cac4d6d4eed12ee9736
Thon Ly <thonly@heartbank.ceo>
2020-09-02 20:44:19 -0700

    finish sw-countdown element

--- elements/sw-countdown.mjs
+++ elements/sw-countdown.mjs
@@ -22,22 +22,29 @@ export class SwCountDown extends HTMLElement {
     }
 
     start() {
-        const hours = this.querySelector("span:nth-child(1)")
-        const minutes = this.querySelector("span:nth-child(2)")
-        const seconds = this.querySelector("span:nth-child(3)")
+        const hours = this.querySelector("h4 span:nth-child(1)")
+        const minutes = this.querySelector("h4 span:nth-child(2)")
+        const seconds = this.querySelector("h4 span:nth-child(3)")
+        const days = this.querySelector("h3 span:nth-child(1)")
+        const daysWord = this.querySelector("h3 span:nth-child(2)")
 
         //console.log(hours, minutes, seconds)
 
         const timer = new SetInterval(() => {
-            if (hours.textContent == 0 && minutes.textContent == 0 && seconds.textContent == 0) {
+            if (days.textContent == 0 && hours.textContent == 0 && minutes.textContent == 0 && seconds.textContent == 0) {
                 timer.stop()
             }
             else {
                 if (seconds.textContent == 0) {
                     seconds.textContent = 60
                     if (minutes.textContent == 0) {
-                        hours.textContent = Number(hours.textContent) - 1
                         minutes.textContent = 60
+                        if (hours.textContent == 0) {
+                            days.textContent = Number(days.textContent) - 1
+                            hours.textContent = 24
+                            daysWord.textContent = days.textContent == 1 ? "day" : "days"
+                        }
+                        hours.textContent = Number(hours.textContent) - 1
                     }
                     minutes.textContent = Number(minutes.textContent) - 1
                 }
